79f464cefacce73d561df7e21ea586f163901489
Tulika Sengupta <tulika44@gmail.com>
2017-09-01 02:03:01 -0400

    SimpleMessenger

---
+++ app/src/main/java/edu/buffalo/cse/cse486586/simplemessenger/SimpleMessengerActivity.java
@@ -0,0 +1,194 @@
+package edu.buffalo.cse.cse486586.simplemessenger;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.KeyEvent;
+        import android.view.View;
+        import android.view.View.OnKeyListener;
+        import android.widget.EditText;
+        import android.widget.TextView;
+
+public class SimpleMessengerActivity extends ActionBarActivity {
+
+    static final String TAG = MainActivity.class.getSimpleName();
+    static final String REMOTE_PORT0 = "11108";
+    static final String REMOTE_PORT1 = "11112";
+    static final int SERVER_PORT = 10000;
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TelephonyManager tel = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        String portStr = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
+        final String myPort = String.valueOf((Integer.parseInt(portStr) * 2));
+
+        try {
+            /*
+             * Create a server socket as well as a thread (AsyncTask) that listens on the server
+             * port.
+             *
+             * AsyncTask is a simplified thread construct that Android provides. Please make sure
+             * you know how it works by reading
+             * http://developer.android.com/reference/android/os/AsyncTask.html
+             */
+            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+            new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
+        } catch (IOException e) {
+            /*
+             * Log is a good way to debug your code. LogCat prints out all the messages that
+             * Log class writes.
+             *
+             * Please read http://developer.android.com/tools/debugging/debugging-projects.html
+             * and http://developer.android.com/tools/debugging/debugging-log.html
+             * for more information on debugging.
+             */
+            Log.e(TAG, "Can't create a ServerSocket");
+            return;
+        }
+
+        final EditText editText = (EditText) findViewById(R.id.edit_text);
+
+
+        editText.setOnKeyListener(new OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    /*
+                     * If the key is pressed (i.e., KeyEvent.ACTION_DOWN) and it is an enter key
+                     * (i.e., KeyEvent.KEYCODE_ENTER), then we display the string. Then we create
+                     * an AsyncTask that sends the string to the remote AVD.
+                     */
+                    String msg = editText.getText().toString() + "\n";
+                    editText.setText(""); // This is one way to reset the input box.
+                    TextView localTextView = (TextView) findViewById(R.id.local_text_display);
+                    localTextView.append("\t" + msg); // This is one way to display a string.
+                    TextView remoteTextView = (TextView) findViewById(R.id.remote_text_display);
+                    remoteTextView.append("\n");
+
+                    /*
+                     * Note that the following AsyncTask uses AsyncTask.SERIAL_EXECUTOR, not
+                     * AsyncTask.THREAD_POOL_EXECUTOR as the above ServerTask does. To understand
+                     * the difference, please take a look at
+                     * http://developer.android.com/reference/android/os/AsyncTask.html
+                     */
+                    new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg, myPort);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    private class ServerTask extends AsyncTask<ServerSocket, String, Void> {
+
+        @Override
+        protected Void doInBackground(ServerSocket... sockets) {
+            ServerSocket serverSocket = sockets[0];
+
+            /*
+             * TODO: Fill in your server code that receives messages and passes them
+             * to onProgressUpdate().
+             */
+
+            Socket socket = null;
+            String msg;
+
+            do {
+                try {
+                    // Accept Connection and Initialize Input Stream
+                    socket = serverSocket.accept();
+                    ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+
+                    msg = (String) input.readObject();
+                    publishProgress(msg);
+                } catch (IOException e) {
+                    Log.e(TAG, "ServerTask IOException");
+                } catch (ClassNotFoundException e) {
+                    Log.e(TAG, "ServerTask ClassNotFoundException");
+                }
+            } while(!socket.isInputShutdown());
+            return null;
+        }
+
+        protected void onProgressUpdate(String...strings) {
+            /*
+             * The following code displays what is received in doInBackground().
+             */
+            String strReceived = strings[0].trim();
+            TextView remoteTextView = (TextView) findViewById(R.id.remote_text_display);
+            remoteTextView.append(strReceived + "\t\n");
+            TextView localTextView = (TextView) findViewById(R.id.local_text_display);
+            localTextView.append("\n");
+
+            /*
+             * The following code creates a file in the AVD's internal storage and stores a file.
+             *
+             * For more information on file I/O on Android, please take a look at
+             * http://developer.android.com/training/basics/data-storage/files.html
+             */
+
+            String filename = "SimpleMessengerOutput";
+            String string = strReceived + "\n";
+            FileOutputStream outputStream;
+
+            try {
+                outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+                outputStream.write(string.getBytes());
+                outputStream.close();
+            } catch (Exception e) {
+                Log.e(TAG, "File write failed");
+            }
+
+            return;
+        }
+    }
+
+    private class ClientTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... msgs) {
+            try {
+                String remotePort = REMOTE_PORT0;
+                if (msgs[1].equals(REMOTE_PORT0))
+                    remotePort = REMOTE_PORT1;
+
+                Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}),
+                        Integer.parseInt(remotePort));
+
+                String msgToSend = msgs[0];
+                /*
+                 * TODO: Fill in your client code that sends out a message.
+                 */
+
+
+                ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+
+                output.writeObject(msgToSend);
+                output.flush();
+                socket.close();
+                socket.close();
+            } catch (UnknownHostException e) {
+                Log.e(TAG, "ClientTask UnknownHostException");
+            } catch (IOException e) {
+                Log.e(TAG, "ClientTask socket IOException");
+            }
+
+            return null;
+        }
+    }
+}
